02ce1baa68d7adf5ddb212d67914ffbb2795b056
adam <adamac92@googlemail.com>
2018-06-02 11:19:12 +0100

    initial app commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const path = require('path');
+
+var app = express();
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+console.log(`server up on ${port}`);
+
+//use index.html page
+app.use(express.static(publicPath));
+
+app.listen(3000);
