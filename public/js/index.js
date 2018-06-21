e1bd6d6beb52a81a7eb02347e421057d76e1b2dd
adam <adamac92@googlemail.com>
2018-06-21 15:18:32 +0100

    css added

--- public/js/index.js
+++ public/js/index.js
@@ -20,14 +20,29 @@ socket.on('newMessage', function(message) {
 	jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+	var li = jQuery('<li></li>');
+	var a = jQuery('<a target="_blank">My Current Location</a>');
+
+	li.text(`${message.from}: `);
+	a.attr('href', message.url);
+
+	li.append(a);
+	jQuery('#messages').append(li);
+});
+
 //grab item by id
 jQuery('#message-form').on('submit', function(e) {
 	e.preventDefault();
+
+	var messageTextbox = jQuery('[name=message]');
+
 	socket.emit('createMessage', {
 		from: 'User',
-		text: jQuery('[name=message]').val()
+		text: messageTextbox.val()
 	}, function () {
-
+		//clear chat box
+		messageTextbox.val('')
 	});
 });
 
@@ -37,12 +52,17 @@ locationButton.on('click', function () {
 		return alert('no location data');
 	}
 
+	locationButton.attr('disabled', 'disabled').text('Sending Location...');
+
 	navigator.geolocation.getCurrentPosition(function (position) {
+		locationButton.removeAttr('disabled');
+		locationButton.text('Send Location');
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		});
 	}, function () {
 		alert('Unable to fetch loc data');
+		locationButton.removAttr('disabled').text('Send location');
 	})
 });
