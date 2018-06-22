72bd08b8c541713efb8c6ae395c245ef8e8be1e0
adam <adamac92@googlemail.com>
2018-06-22 10:46:24 +0100

    moustache.js added for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -12,23 +12,31 @@ socket.on('disconnect', function() {
 //custom events
 
 socket.on('newMessage', function(message) {
-	console.log('new message: ', message);
-	var li = jQuery('<li></li>');
-	li.text(`${message.from}: ${message.text}`);
 
-	//append the message to the list id"messages"
-	jQuery('#messages').append(li);
+	var formattedTime = moment(message.createdAt).format('h:mm a');
+	var template = jQuery('#message-template').html();
+	var html = Mustache.render(template, {
+		text: message.text,
+		from: message.from,
+		createdAt: formattedTime
+	});
+
+	jQuery('#messages').append(html);
+
 });
 
 socket.on('newLocationMessage', function (message) {
-	var li = jQuery('<li></li>');
-	var a = jQuery('<a target="_blank">My Current Location</a>');
+	var formattedTime = moment(message.createdAt).format('h:mm a');
 
-	li.text(`${message.from}: `);
-	a.attr('href', message.url);
+	var template = jQuery('#location-message-template').html();
 
-	li.append(a);
-	jQuery('#messages').append(li);
+	var html = Mustache.render(template, {
+		from: message.from,
+		createdAt: formattedTime,
+		url: message.url
+	});
+
+	jQuery('#messages').append(html);
 });
 
 //grab item by id
